e7323c860381f92114176c308ede8c142382922f
Netanel Gilad <netanelg@wix.com>
2019-05-16 15:11:51 +0300

    implement npm republish

---
+++ index.js
@@ -0,0 +1,31 @@
+const { directory } = require('tempy');
+const { execSync, spawnSync } = require('child_process');
+const { extract } = require('tar');
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+async function republishPackage(originPackageIdentifier, targetVersion, publishArgs) {
+    const tempDir = directory();
+
+    const tarFile = execSync(`npm pack ${originPackageIdentifier}`, {
+        cwd: tempDir
+    });
+    
+    await extract({
+        file: join(tempDir, tarFile.toString().trim()),
+        cwd: tempDir
+    });
+
+    const packageJson = JSON.parse(readFileSync(join(tempDir, 'package/package.json'), 'utf8'));
+    packageJson.version = targetVersion;
+    writeFileSync(join(tempDir, 'package/package.json'), JSON.stringify(packageJson));
+
+    spawnSync('npm', ['publish', ...publishArgs], {
+        cwd: join(tempDir, 'package'),
+        stdio: 'inherit'
+    });
+}
+
+republishPackage(process.argv[2], process.argv[3], process.argv.slice(4));
+
+
---
+++ index.spec.js
@@ -0,0 +1,97 @@
+const { spawnSync, spawn, execSync } = require("child_process");
+const { directory } = require("tempy");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+
+let verdaccioProcess;
+
+beforeAll(() => { 
+    verdaccioProcess = spawn("npx", ["verdaccio", "--config", "verdaccio.yaml"]);
+    spawnSync("npx", ["wait-port", "4873", "-o", "silent"]);
+});
+
+afterAll(() => {
+    verdaccioProcess.kill();
+});
+
+afterEach(() => {
+    spawnSync("rm", ["-rf", "./storage"]);
+});
+
+test("should republish an existing package using the preconfigured npm registry", async () => {
+    publishCheckPackage()
+
+    const { stdout } = spawnSync(
+        "sh",
+        ["-c", "node ./index.js check-package@1.0.0 1.1.0"],
+        {
+            env: {
+                ...process.env,
+                NPM_CONFIG_REGISTRY: "http://localhost:4873"
+            }
+        }
+    );
+
+    const { stdout: packageDef } = spawnSync("npm", [
+        "view",
+        "check-package",
+        "--registry",
+        "http://localhost:4873",
+        "-json",
+        "versions"
+    ]);
+
+    expect(JSON.parse(packageDef.toString())).toContain("1.1.0");
+    expect(stdout.toString()).toMatchInlineSnapshot(`
+    "+ check-package@1.1.0
+    "
+  `);
+});
+
+test("should republish an existing package and pass publish args", async () => {
+    publishCheckPackage()
+
+    const { stdout } = spawnSync(
+        "sh",
+        ["-c", "node ./index.js check-package@1.0.0 1.1.0 --tag my-tag"],
+        {
+            env: {
+                ...process.env,
+                NPM_CONFIG_REGISTRY: "http://localhost:4873"
+            }
+        }
+    );
+
+    const { stdout: packageDefBuffer } = spawnSync("npm", [
+        "view",
+        "check-package",
+        "--registry",
+        "http://localhost:4873",
+        "-json"
+    ]);
+
+    const packageDef = JSON.parse(packageDefBuffer.toString());
+
+    expect(packageDef.versions).toContain("1.1.0");
+    expect(packageDef['dist-tags']['my-tag']).toEqual('1.1.0')
+    expect(stdout.toString()).toMatchInlineSnapshot(`
+      "+ check-package@1.1.0
+      "
+    `);
+});
+
+function publishCheckPackage() {
+    const tempDir = directory();
+
+    writeFileSync(
+        join(tempDir, "package.json"),
+        JSON.stringify({
+            name: "check-package",
+            version: "1.0.0"
+        })
+    );
+
+    spawnSync("npm", ["publish", "--registry", "http://localhost:4873"], {
+        cwd: tempDir
+    });
+}
