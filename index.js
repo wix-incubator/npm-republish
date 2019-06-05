f83aa624a4ccadca346d2d527529ad55394baa29
Netanel Gilad <netanelg@wix.com>
2019-06-05 10:24:47 +0300

    support node > 6

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ const { extract } = require('tar');
 const { readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
-async function republishPackage(originPackageIdentifier, targetVersion, publishArgs) {
+function republishPackage(originPackageIdentifier, targetVersion, publishArgs) {
     const tempDir = directory();
 
-    const tarFile = execSync(`npm pack ${originPackageIdentifier}`, {
-        cwd: tempDir
-    });
-    
-    await extract({
-        file: join(tempDir, tarFile.toString().trim()),
-        cwd: tempDir
-    });
+    return Promise.resolve()
+        .then(() => {
+            const tarFile = execSync(`npm pack ${originPackageIdentifier}`, {
+                cwd: tempDir
+            });
 
-    const packageJson = JSON.parse(readFileSync(join(tempDir, 'package/package.json'), 'utf8'));
-    packageJson.version = targetVersion;
-    writeFileSync(join(tempDir, 'package/package.json'), JSON.stringify(packageJson));
+            return extract({
+                file: join(tempDir, tarFile.toString().trim()),
+                cwd: tempDir
+            });
+        })
+        .then(() => {
+            const packageJson = JSON.parse(readFileSync(join(tempDir, 'package/package.json'), 'utf8'));
+            packageJson.version = targetVersion;
+            writeFileSync(join(tempDir, 'package/package.json'), JSON.stringify(packageJson));
 
-    spawnSync('npm', ['publish', ...publishArgs], {
-        cwd: join(tempDir, 'package'),
-        stdio: 'inherit'
-    });
+            spawnSync('npm', ['publish', ...publishArgs], {
+                cwd: join(tempDir, 'package'),
+                stdio: 'inherit'
+            });
+        });
 }
 
 module.exports = {
