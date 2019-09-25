15c1bc0afb71f14caf6d5142c8382b974d7127cd
Netanel Gilad <netanelg@wix.com>
2019-09-25 23:31:34 +0300

    handle existing package errors in the registry

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
 const { extract } = require('tar');
 const { readFileSync, writeFileSync, mkdirSync } = require('fs');
 const { join } = require('path');
@@ -11,6 +11,22 @@ function tempDirectory() {
     const directory = getPath();
     mkdirSync(directory);
     return directory;
+}
+
+function stringHasForbiddenCantPublishBecauseVersionExists(str) {
+    return (
+        str.indexOf('forbidden cannot modify pre-existing version') > -1 || // artifactory error
+        str.indexOf('cannot publish over the previously published versions') > -1 // npmjs error
+    );
+}
+
+function getPackageVersionInfo(registry, packageName, version) {
+    return JSON.parse(
+        execSync(
+            `npm show --json --registry=${registry} ${packageName}@${version}`,
+            { stdio: 'pipe' },
+        ).toString(),
+    );
 }
 
 /**
@@ -29,6 +45,8 @@ function republishPackage(originPackageIdentifier, targetVersion, publishArgs, r
                 cwd: tempDir
             });
 
+            console.log('Finished downloading origin opackage tarball, extracting...');
+
             return extract({
                 file: join(tempDir, tarFile.toString().trim()),
                 cwd: tempDir
@@ -38,15 +56,32 @@ function republishPackage(originPackageIdentifier, targetVersion, publishArgs, r
             console.log('Finished downloading and extracting the origin package.');
             const packageJson = JSON.parse(readFileSync(join(tempDir, 'package/package.json'), 'utf8'));
             packageJson.version = targetVersion;
-            packageJson.uniqeBuildIdentifier = uniqueString();
-            console.log('Unique identifier for this publish', packageJson.uniqeBuildIdentifier);
+            packageJson.uniqePublishIdentifier = uniqueString();
+            console.log('Unique identifier for this publish', packageJson.uniqePublishIdentifier);
             writeFileSync(join(tempDir, 'package/package.json'), JSON.stringify(packageJson));
             console.log(`Wrote the target version ${targetVersion} to the package.json`);
 
-            execSync(`npm publish --ddd --ignore-scripts ${publishArgs.join(' ')}`, {
+            const subProcess = exec(`npm publish --ddd --ignore-scripts ${publishArgs.join(' ')}`, {
                 cwd: join(tempDir, 'package'),
-                stdio: 'inherit'
+            }, (error, stdout, stderr) => {
+                if (error) {
+                    if (stringHasForbiddenCantPublishBecauseVersionExists(stdout) ||
+                        stringHasForbiddenCantPublishBecauseVersionExists(stderr)) {
+                        const versionInfo = getPackageVersionInfo(registry, packageJson.name, targetVersion);
+                        if (versionInfo.uniqePublishIdentifier !== packageJson.uniqePublishIdentifier) {
+                            console.error(error);
+                            process.exit(error.code);
+                        }
+                    }
+                    else {
+                        console.error(error);
+                        process.exit(error.code);
+                    }
+                }
             });
+
+            subProcess.stderr.pipe(process.stderr);
+            subProcess.stdout.pipe(process.stdout);
 
             console.log('Publish to target version succeeded.');
         });
